7508a37b163f54f2d3a69661e154b91fd417d683
Lisa Helminger <lisa-maria.helminger@uibk.ac.at>
2018-04-11 17:39:18 +0200

    Step1

---
+++ main.js
@@ -0,0 +1,32 @@
+//alert("Hello world");
+
+let nachricht = "Hallo Welt";
+//alert(nachricht);
+nachricht = "neuer Text";
+//alert(nachricht);
+console.log(nachricht);
+const linkColor = "#ff0000";
+let eine_Variable = 'test';
+console.log('Variable: eine_Variable: ', eine_Variable);
+let highScore = 75.994
+console.log("Highscore ist: ", highScore / 20);
+let meineNachricht="Moeglichkeit1"
+console.log(meineNachricht);
+meineNachricht= 'Moeglichkeit2';
+console.log(meineNachricht);
+meineNachricht = `Moeglichkeit3`
+console.log(meineNachricht);
+meineNachricht = `Ein mehr
+und ein 
+ganz langer
+Textund aus`;
+
+console.log(meineNachricht);
+let fullName = 'The" Dude"'
+console.log(fullName);
+
+let isOver18 = true;
+console.log('über 18?', isOver18);
+let age = 17
+console.log('age über 18', age >18);
+
